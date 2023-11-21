2f309edaea31961e60ccf10f1edb3e3d775471f8
Sicheng Yi <sy2199@nyu.edu>
2023-11-21 14:58:24 -0500

    hw9

---
+++ hw9/forbidden-requests.py
@@ -0,0 +1,21 @@
+from google.cloud import pubsub_v1
+
+# listen for banned countries
+subscriber = pubsub_v1.SubscriberClient()
+
+subscription_path = subscriber.subscription_path('ds561-trial-project', 'banned-countries-sub')
+
+def callback(message):
+  print('Received message: {}'.format(message.data.decode('utf-8')))
+  message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+
+print('Listening for banned countries...')
+
+with subscriber:
+  try:
+    streaming_pull_future.result()
+  except TimeoutError:
+    streaming_pull_future.cancel()
+    streaming_pull_future.result()
